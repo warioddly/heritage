5671becd2c2fe74565b8761ad2a5d96d0f36c8bb
warioddly <warioddly@gmail.com>
2024-04-30 16:59:23 +0600

    add google search

--- src/components/tree/InteractiveViewer.tsx
+++ src/components/tree/InteractiveViewer.tsx
@@ -3,35 +3,96 @@ import CytoscapeComponent from 'react-cytoscapejs';
 // @ts-ignore
 import cola from 'cytoscape-cola';
 import cytoscape from "cytoscape";
+import {useState} from "react";
+
+
 
 
 export function TreeInteractiveViewer() {
+
   cytoscape.use(cola);
 
-  const elements = [
-    { data: { id: 'one', label: 'Node 1' }, position: { x: 0, y: 0 } },
-    { data: { id: 'two', label: 'Node 2' }, position: { x: 100, y: 0 } },
-    { data: { source: 'one', target: 'two', label: 'Edge from Node1 to Node2' } }
-  ];
+  const [width, setWith] = useState('100vw');
+  const [height, setHeight] = useState('100vh');
+  const [graphData, setGraphData] = useState({
+    nodes: [
+      { data: { id: '1', label: 'IP 1', type: 'ip' } },
+      { data: { id: '2', label: 'Device 1', type: 'device' } },
+      { data: { id: '3', label: 'IP 2', type: 'ip' } },
+      { data: { id: '4', label: 'Device 2', type: 'device' } },
+      { data: { id: '5', label: 'Device 3', type: 'device' } },
+      { data: { id: '6', label: 'IP 3', type: 'ip' } },
+      { data: { id: '7', label: 'Device 5', type: 'device' } },
+      { data: { id: '8', label: 'Device 6', type: 'device' } },
+      { data: { id: '9', label: 'Device 7', type: 'device' } },
+      { data: { id: '10', label: 'Device 8', type: 'device' } },
+      { data: { id: '11', label: 'Device 9', type: 'device' } },
+      { data: { id: '12', label: 'IP 3', type: 'ip' } },
+      { data: { id: '13', label: 'Device 10', type: 'device' } },
+    ],
+    edges: [
+      {
+        data: { source: '1', target: '2', label: 'Node2' },
+      },
+      {
+        data: { source: '3', target: '4', label: 'Node4' },
+      },
+      {
+        data: { source: '3', target: '5', label: 'Node5' },
+      },
+      {
+        data: { source: '6', target: '5', label: ' 6 -> 5' },
+      },
+      {
+        data: { source: '6', target: '7', label: ' 6 -> 7' },
+      },
+      {
+        data: { source: '6', target: '8', label: ' 6 -> 8' },
+      },
+      {
+        data: { source: '6', target: '9', label: ' 6 -> 9' },
+      },
+      {
+        data: { source: '3', target: '13', label: ' 3 -> 13' },
+      },
+    ],
+  });
+
+  // const layout = {
+  //   name: 'breadthfirst',
+  //   fit: true,
+  //   // circle: true,
+  //   directed: true,
+  //   padding: 50,
+  //   // spacingFactor: 1.5,
+  //   animate: true,
+  //   animationDuration: 1000,
+  //   avoidOverlap: true,
+  //   nodeDimensionsIncludeLabels: false,
+  // };
 
   const layout = {
     name: 'cola',
     animate: true, // whether to show the layout as it's running
-    refresh: 2,
-    maxSimulationTime: 4000,
-    ungrabifyWhileSimulating: true,
+    refresh: 1, // number of ticks per frame; higher is faster but more jerky
+    maxSimulationTime: 4000, // max length in ms to run the layout
+    ungrabifyWhileSimulating: false, // so you can't drag nodes during layout
     fit: true, // on every layout reposition of nodes, fit the viewport
     padding: 30, // padding around the simulation
     boundingBox: undefined, // constrain layout bounds; { x1, y1, x2, y2 } or { x1, y1, w, h }
-    nodeDimensionsIncludeLabels: true, // whether labels should be included in determining the space used by a node
+    nodeDimensionsIncludeLabels: false, // whether labels should be included in determining the space used by a node
+
+    // layout event callbacks
     ready: function () {}, // on layoutready
     stop: function () {}, // on layoutstop
+
+    // positioning options
     randomize: false, // use random node positions at beginning of layout
     avoidOverlap: true, // if true, prevents overlap of node bounding boxes
     handleDisconnected: true, // if true, avoids disconnected components from overlapping
-    convergenceThreshold: 22.01, // when the alpha value (system energy) falls below this value, the layout stops
-    nodeSpacing: function (node: any) {
-      return 22;
+    convergenceThreshold: 0.01, // when the alpha value (system energy) falls below this value, the layout stops
+    nodeSpacing: function (node) {
+      return 10;
     }, // extra spacing around nodes
     flow: undefined, // use DAG/tree flow layout if specified, e.g. { axis: 'y', minSeparation: 30 }
     alignment: undefined, // relative alignment constraints on nodes, e.g. {vertical: [[{node: node1, offset: 0}, {node: node2, offset: 5}]], horizontal: [[{node: node3}, {node: node4}], [{node: node5}, {node: node6}]]}
@@ -50,11 +111,90 @@ export function TreeInteractiveViewer() {
     allConstIter: undefined, // initial layout iterations with all constraints including non-overlap
   };
 
-  return (<>
-    <CytoscapeComponent
-        layout={layout}
-        elements={elements}
-        style={ { width: '100vw', height: '100vh' } }
-    />
-  </>)
+  const styleSheet = [
+    {
+      selector: 'node',
+      style: {
+        backgroundColor: '#4a56a6',
+        width: 30,
+        height: 30,
+        label: 'data(label)',
+
+        // "width": "mapData(score, 0, 0.006769776522008331, 20, 60)",
+        // "height": "mapData(score, 0, 0.006769776522008331, 20, 60)",
+        // "text-valign": "center",
+        // "text-halign": "center",
+        'overlay-padding': '6px',
+        'z-index': '10',
+        //text props
+        'text-outline-color': '#4a56a6',
+        'text-outline-width': '2px',
+        color: 'white',
+        fontSize: 20,
+      },
+    },
+    {
+      selector: 'node:selected',
+      style: {
+        'border-width': '6px',
+        'border-color': '#AAD8FF',
+        'border-opacity': '0.5',
+        'background-color': '#77828C',
+        width: 50,
+        height: 50,
+        //text props
+        'text-outline-color': '#77828C',
+        'text-outline-width': 8,
+      },
+    },
+    {
+      selector: "node[type='device']",
+      style: {
+        shape: 'rectangle',
+      },
+    },
+    {
+      selector: 'edge',
+      style: {
+        width: 3,
+        // "line-color": "#6774cb",
+        'line-color': '#AAD8FF',
+        'target-arrow-color': '#6774cb',
+        'target-arrow-shape': 'triangle',
+        'curve-style': 'bezier',
+      },
+    },
+  ];
+
+  let myCyRef;
+
+  return (
+      <>
+        <CytoscapeComponent
+            elements={CytoscapeComponent.normalizeElements(graphData)}
+            // pan={{ x: 200, y: 200 }}
+            style={{ width: width, height: height }}
+            zoomingEnabled={true}
+            maxZoom={3}
+            minZoom={1}
+            autounselectify={false}
+            boxSelectionEnabled={true}
+            layout={layout}
+            stylesheet={styleSheet}
+            cy={(cy) => {
+              myCyRef = cy;
+
+              console.log('EVT', cy);
+
+              cy.on('tap', 'node', (evt) => {
+                var node = evt.target;
+                console.log('EVT', evt);
+                console.log('TARGET', node.data());
+                console.log('TARGET TYPE', typeof node[0]);
+              });
+            }}
+            abc={console.log('myCyRef', myCyRef)}
+        />
+      </>
+  );
 }
