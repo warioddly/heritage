2a6f4efed353b54728e360d51274a731d784299b
warioddly <53710487+warioddly@users.noreply.github.com>
2024-05-01 15:23:17 +0600

    create InteractiveViewer.tsx

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,6 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./node_modules/flowbite-react/lib/**/*.js",
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
