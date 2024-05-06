fb07e2d3cf86e895bde433ee5f91b71b35c55238
warioddly <warioddly@gmail.com>
2024-05-06 18:16:17 +0600

    Add dart CLI https://www.sanjyra.net/ site parser

---
+++ sanjyra-parser/bin/sanjyra_parser.dart
@@ -0,0 +1,5 @@
+import 'package:sanjyra_parser/sanjyra_parser.dart' as sanjyra_parser;
+
+void main(List<String> arguments) {
+  print('Hello world: ${sanjyra_parser.calculate()}!');
+}
---
+++ sanjyra-parser/test/sanjyra_parser_test.dart
@@ -0,0 +1,8 @@
+import 'package:sanjyra_parser/sanjyra_parser.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
